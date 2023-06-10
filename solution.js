da2591aac2e8eecbeef93eaa080b990d5e0adc20
Laura Wu <leemaiwu@gmail.com>
2023-06-10 12:11:12 -0600

    Added alternate one line solution

--- solution.js
+++ solution.js
@@ -5,3 +5,12 @@ const minMax = arr => {
 
 console.log(minMax([1, 5])) // [1, 5]
 console.log(minMax([2334454, 55, 5])) // [5, 2334454]
+
+
+// Alternate Solution
+
+
+const minMaxs = arr => [ Math.min(...arr), Math.max(...arr) ]
+  
+console.log(minMaxs([1, 5])) // [1, 5]
+console.log(minMaxs([2334454, 55, 5])) // [5, 2334454]
