f5dbfc1110a99ed96c9302b14f98edeb5582aac9
Laura Wu <leemaiwu@gmail.com>
2023-06-10 12:08:47 -0600

    Completed with sort method

--- solution.js
+++ solution.js
@@ -1,4 +1,7 @@
 const minMax = arr => {
-  return [0,0]
+  sortedArr = arr.sort((a, b) => a - b)
+  return [sortedArr[0], sortedArr[sortedArr.length - 1]]
 }
 
+console.log(minMax([1, 5])) // [1, 5]
+console.log(minMax([2334454, 55, 5])) // [5, 2334454]
